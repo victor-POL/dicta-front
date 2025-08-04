2f94cac41ffb4458ab913af0f5dbef23e1eeb844
Victor <99147005+victor-POL@users.noreply.github.com>
2025-08-04 15:45:57 -0300

    feat: react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import Paneles from './components/Paneles';
+import { Route, Routes } from 'react-router'
+import Paneles from './components/Paneles'
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1 style={{ color: 'white' }}>Diseño con paneles</h1>
-      <Paneles />
-    </div>
-  );
+    <Routes>
+      <Route
+        path="/herramientas"
+        element={
+          <div style={{ padding: '20px' }}>
+            <h1 style={{ color: 'white' }}>Diseño con paneles</h1>
+            <Paneles />
+          </div>
+        }
+      />
+      <Route path="*" element={<div>Página no encontrada</div>} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
