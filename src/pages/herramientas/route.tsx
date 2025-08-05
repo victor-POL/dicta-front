e72df23816210473b4d68145d6a676c215afa8a7
Victor <99147005+victor-POL@users.noreply.github.com>
2025-08-04 23:51:10 -0300

    fix: padding innecesario en page herramientas

--- src/pages/herramientas/route.tsx
+++ src/pages/herramientas/route.tsx
@@ -1,12 +1,7 @@
 import Paneles from '@/components/Paneles'
 
 const HerramientasPage = () => {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1 style={{ color: 'white' }}>Diseño con paneles</h1>
-      <Paneles />
-    </div>
-  )
+  return <Paneles />
 }
 
 export default HerramientasPage
